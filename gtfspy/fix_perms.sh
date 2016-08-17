694cb03bd98de6fb59708c5080205f883f719fa2
Richard Darst <rkd@zgib.net>
2016-08-17 15:58:15 +0300

    Rearrange files into directories
    - gtfspy/: Main library
    - web/: web-related visualizations
    - analysis/: Our specific analysis
    - pipeline/: Things that orchestrate our particular data pipeline

--- gtfspy/fix_perms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=/proj/networks/darst/hackathon/
-
-echo $DIR
-#chmod g+rwX $DIR
-find $DIR -name .git -prune -or -type d -exec echo chmod g+s {} \;
--- gtfspy/get_hsl_disruption_data.py
+++
@@ -1,7 +0,0 @@
-import requests
-import xml.etree.ElementTree as ET
-
-req = requests.get('http://www.poikkeusinfo.fi/xml/v2/210120161220')
-et = ET.fromstring(req.text.encode('utf-8'))
-print ET.tostring(et, encoding='utf8', method='xml')
-
