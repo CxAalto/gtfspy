f57785785bd7eac8302830548362f1df6224065c
Rainer Kujala <rainer.kujala@gmail.com>
2017-03-30 13:16:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
-from distutils.core import setup
+from setuptools import setup
 
 from Cython.Build import cythonize
 
-version="0.0.1"
+version="0.0.1.dev1"
 
 setup(
-    name="gfspy",
-    packages=["gtfspy"],
+    name="gtfspy",
     version=version,
     description="Python package for analyzing public transport timetables",
+    url="https://github.com/CxAalto/gtfspy",
+    packages=["gtfspy"],
     author="Rainer Kujala",
     author_email="Rainer.Kujala@gmail.com",
-    url="https://github.com/CxAalto/gtfspy",
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ],
     download_url="https://github.com/CxAalto/gtfspy/archive/" + version + ".tar.gz",
     ext_modules=cythonize("gtfspy/routing/*.pyx"),
     keywords = ['transit', 'routing' 'gtfs', 'public transport', 'analysis', 'visualization'], # arbitrary keywords
