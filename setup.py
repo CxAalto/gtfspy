e3f29ce539f5074e8d87225c871ae6a89985355b
Rainer Kujala <rainer.kujala@gmail.com>
2017-03-30 10:13:51 +0300

    gtfspy 0.0.1

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from distutils.core import setup
 
 from Cython.Build import cythonize
 
+version="0.0.1"
+
 setup(
     name="gfspy",
     packages=["gtfspy"],
-    version="0.1",
+    version=version,
     description="Python package for analyzing public transport timetables",
     author="Rainer Kujala",
     author_email="Rainer.Kujala@gmail.com",
     url="https://github.com/CxAalto/gtfspy",
-    download_url="https://github.com/CxAalto/gtfspy/archive/0.2.tar.gz",
+    download_url="https://github.com/CxAalto/gtfspy/archive/" + version + ".tar.gz",
     ext_modules=cythonize("gtfspy/routing/*.pyx"),
     keywords = ['transit', 'routing' 'gtfs', 'public transport', 'analysis', 'visualization'], # arbitrary keywords
 )
