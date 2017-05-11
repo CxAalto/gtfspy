86b361db9c71fd8a8281753ffe1be941e0ac4797
jouabes1 <sami.jouaber@gmail.com>
2017-05-11 16:33:52 +0300

    Raise an error when filtering by date with missing information

--- gtfspy/filter.py
+++ gtfspy/filter.py
@@ -136,6 +136,8 @@ class FilterExtract(object):
         if (self.start_date is not None) and (self.end_date is not None):
             start_date_ut = self.gtfs.get_day_start_ut(self.start_date)
             end_date_ut = self.gtfs.get_day_start_ut(self.end_date)
+            if self.copy_db_conn.execute("select count(*) from day_trips2 where start_time_ut is null or end_time_ut is null").fetchone() != (0,):
+                raise ValueError("Missing information in day_trips2 (start_time_ut and/or end_time_ut), check trips.start_time_ds and trips.end_time_ds.")
             logging.info("Filtering based on agency_ids")
             # negated from import_gtfs
             table_to_remove_map = {
