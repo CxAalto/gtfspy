b153f96c7ae8ed7e025a2ace3e72a1b10cda8452
Rainer Kujala <rainer.kujala@gmail.com>
2017-07-28 16:08:06 +0300

    Add possibility for interpolating temporal distances

--- gtfspy/routing/profile_block_analyzer.py
+++ gtfspy/routing/profile_block_analyzer.py
@@ -1,48 +1,8 @@
-from collections import namedtuple
-
-import numpy
 from collections import defaultdict
 
+import numpy
 
-class ProfileBlock():
-
-    def __init__(self, start_time, end_time, distance_start, distance_end, **extra_properties):
-        self.start_time = start_time
-        self.end_time = end_time
-        assert(self.start_time < self.end_time)
-        self.distance_start = distance_start
-        self.distance_end = distance_end
-        self.extra_properties = extra_properties
-
-    def area(self):
-        return self.width() * self.mean()
-
-    def mean(self):
-        return 0.5 * (self.distance_start + self.distance_end)
-
-    def width(self):
-        return self.end_time - self.start_time
-
-    def max(self):
-        return max(self.distance_start, self.distance_end)
-
-    def min(self):
-        return min(self.distance_start, self.distance_end)
-
-    def is_flat(self):
-        return self.distance_start == self.distance_end
-
-    def __getitem__(self, extra_property_name):
-        return self.extra_properties[extra_property_name]
-
-    def __str__(self):
-        parts = []
-        parts.append(self.start_time)
-        parts.append(self.end_time)
-        parts.append(self.distance_start)
-        parts.append(self.distance_end)
-        parts.append(self.extra_properties)
-        return str(parts)
+from gtfspy.routing.profile_block import ProfileBlock
 
 
 class ProfileBlockAnalyzer:
@@ -51,7 +11,7 @@ class ProfileBlockAnalyzer:
         """
         Parameters
         ----------
-        profile_blocks: list[ProfileBlock]
+        profile_blocks: list[gtfspy.routing.profile_block.ProfileBlock]
         """
         for i, block in enumerate(profile_blocks[:-1]):
             assert block.start_time < block.end_time
@@ -273,3 +233,11 @@ class ProfileBlockAnalyzer:
 
     def get_blocks(self):
         return self._profile_blocks
+
+    def interpolate(self, time):
+        assert(self._start_time <= time <= self._end_time)
+        for profile_block in self._profile_blocks:
+            # find the first block whose end time is larger than or equal to that of the queried time
+            if profile_block.end_time >= time:
+                return profile_block.interpolate(time)
+
