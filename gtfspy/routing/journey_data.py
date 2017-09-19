7c66628fd58cbcd89efa4ab56950a6a00d70a291
Rainer Kujala <rainer.kujala@gmail.com>
2017-09-19 15:02:20 +0300

    Adjust logging

--- gtfspy/routing/journey_data.py
+++ gtfspy/routing/journey_data.py
@@ -555,7 +555,8 @@ class JourneyDataManager:
         _flush_data_to_db(results_dict)
 
         for i, (origin, target, journey_labels) in enumerate(self._journey_label_generator(targets, origins)):
-            print("\r", i, "/", n_pairs_tot, " : ", "%.2f" % round(float(i) / n_pairs_tot, 3), end='', flush=True)
+            if i % 100 == 0:
+                print("\r", i, "/", n_pairs_tot, " : ", "%.2f" % round(float(i) / n_pairs_tot, 3), end='', flush=True)
 
             kwargs = {"from_stop_I": origin, "to_stop_I": target}
             walking_distance = self.gtfs.get_stop_distance(origin, target)
