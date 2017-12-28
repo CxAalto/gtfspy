1c02273e9c970539f965c753f20b46d02e3fa5d3
Rainer Kujala <Rainer.Kujala@aalto.fi>
2017-12-28 13:15:37 +0200

    Fix aggragation related adjusting of stop_times

--- gtfspy/aggregate_stops.py
+++ gtfspy/aggregate_stops.py
@@ -28,12 +28,14 @@ def aggregate_stops_spatially(gtfs, threshold_meters=1):
 
     # Find the connected components of that network.
     components = list(networkx.connected_components(g))
+    print([len(component) for component in components])
 
     # For each connected component, choose one representative stop_I, and create a dictionary `to_new_stop_I' mapping original stop_I to the representative stop_I
     to_new_stop_I = dict()
     for component in components:
-        representative_stop_I = next(iter(component))
-        for stop_I in component:
+        component_iter = iter(component)
+        representative_stop_I = next(component_iter)
+        for stop_I in component_iter:
             to_new_stop_I[stop_I] = representative_stop_I
 
     # Remove those stops in `stops` table that are not mapped to themselves in `to_new_stop_I'.
@@ -45,6 +47,7 @@ def aggregate_stops_spatially(gtfs, threshold_meters=1):
     stop_Is_to_remove_list_str = "(" + ",".join([str(stop_I) for stop_I in stop_Is_to_remove]) + ")"
 
     # Remove rows from stops and stop_distances tables where
+    print("    Removing ", len(stop_Is_to_remove), " duplicate stops")
     gtfs.execute_custom_query("DELETE FROM stops WHERE stop_I IN " + stop_Is_to_remove_list_str)
     gtfs.execute_custom_query("DELETE FROM stop_distances WHERE " +
                               "from_stop_I IN " + stop_Is_to_remove_list_str +
@@ -54,7 +57,7 @@ def aggregate_stops_spatially(gtfs, threshold_meters=1):
     to_change = []
     for old_stop_I, new_stop_I in to_new_stop_I.items():
         if old_stop_I != new_stop_I:
-            to_change.append((new_stop_I, old_stop_I))
+            to_change.append((int(new_stop_I), int(old_stop_I)))
     # create a temporary index if there are a lot of changes
     if len(to_change) > 10:
         gtfs.conn.execute("CREATE INDEX IF NOT EXISTS tmp_index_stop_times_stop_I ON stop_times (stop_I)")
@@ -62,3 +65,4 @@ def aggregate_stops_spatially(gtfs, threshold_meters=1):
     gtfs.conn.executemany("UPDATE stop_times SET stop_I=? WHERE stop_I=?", to_change)
     # Remove the temporary index
     gtfs.conn.execute("DROP INDEX IF EXISTS tmp_index_stop_times_stop_I")
+    gtfs.conn.commit()
