0ec630b984ceb752270f506ed0c04af72378c87a
Rainer Kujala <rainer.kujala@gmail.com>
2018-06-08 16:49:30 +0300

    Adjust example codes

--- examples/example_temporal_distance_profile.py
+++ examples/example_temporal_distance_profile.py
@@ -10,16 +10,14 @@ G = example_import.load_or_import_example_gtfs()
 
 from_stop_name = "Ahkiotie 2 E"
 to_stop_name = "Kauppahalli P"
-from_stop_I = None
-to_stop_I = None
+
+from_stop_I = G.get_stop_I_from_name(from_stop_name)
+to_stop_I = G.get_stop_I_from_name(to_stop_name)
+assert(from_stop_I is not None)
+assert(to_stop_I is not None)
+
 stop_dict = G.stops().to_dict("index")
-for stop_I, data in stop_dict.items():
-    if data['name'] == from_stop_name:
-        from_stop_I = stop_I
-    if data['name'] == to_stop_name:
-        to_stop_I = stop_I
-assert (from_stop_I is not None)
-assert (to_stop_I is not None)
+
 
 # The start and end times between which PT operations (and footpaths) are scanned:
 ANALYSIS_START_TIME_UT = G.get_suitable_date_for_daily_extract(ut=True) + 10 * 3600
