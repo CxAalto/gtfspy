973b3f09b1b34de766eda3b3188424a943317288
Christoffer Weckstrom <christoffer.weckstrom@aalto.fi>
2018-12-05 01:16:19 +0200

    gtfspy/import_loaders/trip_loader.py: simplified the process of adding min/max arr/dep times to trip table, fixing an error found among feeds with negative timezone

--- gtfspy/import_loaders/trip_loader.py
+++ gtfspy/import_loaders/trip_loader.py
@@ -57,28 +57,11 @@ class TripLoader(TableLoader):
 
 
 def update_trip_travel_times_ds(conn):
-    cur0 = conn.cursor()
     cur = conn.cursor()
-    cur0.execute('''SELECT trip_I, min(dep_time), max(arr_time)
-                   FROM trips JOIN stop_times USING (trip_I)
-                   GROUP BY trip_I''')
 
-    print("Updating travel times in trips table")
-
-    def iter_rows(cur0):
-        for row in cur0:
-            if row[1]:
-                st = row[1].split(':')
-                start_time_ds = int(st[0]) * 3600 + int(st[1]) * 60 + int(st[2])
-            else:
-                start_time_ds = None
-            if row[2]:
-                et = row[2].split(':')
-                end_time_ds = int(et[0]) * 3600 + int(et[1]) * 60 + int(et[2])
-            else:
-                end_time_ds = None
-            yield start_time_ds, end_time_ds, row[0]
-
-    cur.executemany('''UPDATE trips SET start_time_ds=?, end_time_ds=? WHERE trip_I=?''',
-                    iter_rows(cur0))
+    cur.execute('''UPDATE trips SET (start_time_ds, end_time_ds) = 
+                   (SELECT min(dep_time_ds), max(arr_time_ds) 
+                   FROM stop_times 
+                   WHERE stop_times.trip_I = trips.trip_I 
+                   GROUP BY trip_I)''')
     conn.commit()
