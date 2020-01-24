b068b2f4711195fbfa3f7c7b4ddaae66952fe32b
Michaela Ockova <ockova.michaela@gmail.com>
2020-01-24 21:27:28 +0100

    Add pre-commit configuration

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension, find_packages
 
-version="0.0.4"
+version = "0.0.4"
 
 setup(
     name="gtfspy",
@@ -10,34 +10,33 @@ setup(
     packages=find_packages(exclude=["java_routing", "examples"]),
     author="Rainer Kujala",
     author_email="Rainer.Kujala@gmail.com",
-    license='MIT',
+    license="MIT",
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: GIS',
-
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: GIS",
         # Pick your license as you wish (should match "license" above)
-         'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
-    ], 
-    install_requires = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    setup_requires=["setuptools>=18.0", "cython"],
+    install_requires=[
         "setuptools>=18.0",
         "pandas",
         "networkx==1.11",
         "pyshp",
         "smopy",
-        "nose",
         "Cython",
         "six",
         "geoindex",
@@ -45,13 +44,15 @@ setup(
         "shapely",
         "geojson>=2.0.0",
         "pyproj",
-        "matplotlib-scalebar==0.6.1"
+        "matplotlib-scalebar==0.6.1",
     ],
-    ext_modules=[
-        Extension(
-            'gtfspy.routing.label',
-            sources=["gtfspy/routing/label.pyx"],
-        ),
-    ],
-    keywords = ['transit', 'routing' 'gtfs', 'public transport', 'analysis', 'visualization'], # arbitrary keywords
+    tests_require=["nose"],
+    ext_modules=[Extension("gtfspy.routing.label", sources=["gtfspy/routing/label.pyx"],),],
+    keywords=[
+        "transit",
+        "routing" "gtfs",
+        "public transport",
+        "analysis",
+        "visualization",
+    ],  # arbitrary keywords
 )
