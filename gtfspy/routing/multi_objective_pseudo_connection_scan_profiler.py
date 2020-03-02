dab2bb7397dbc11c37cebcf9c82431d841b452fd
Richard Darst <rkd@zgib.net>
2020-03-03 00:25:55 +0200

    Update networkx methods to networkx==2.4

--- gtfspy/routing/multi_objective_pseudo_connection_scan_profiler.py
+++ gtfspy/routing/multi_objective_pseudo_connection_scan_profiler.py
@@ -294,7 +294,7 @@ class MultiObjectivePseudoCSAProfiler(AbstractRoutingAlgorithm):
             neighbor_label_bags = []
             walk_durations_to_neighbors = []
             departure_arrival_stop_pairs = []
-            if stop_profile.get_walk_to_target_duration() != 0 and stop in self._walk_network.node:
+            if stop_profile.get_walk_to_target_duration() != 0 and stop in self._walk_network.nodes:
                 neighbors = networkx.all_neighbors(self._walk_network, stop)
                 for neighbor in neighbors:
                     neighbor_profile = self._stop_profiles[neighbor]
